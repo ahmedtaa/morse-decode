5d6c0f5d234bb9e7ec2d4304b8b16e0a7d0b5a76
ahmed tawfik ali <a.alrakhawy@gmail.com>
2022-04-07 09:18:50 +0200

    Add decoder.rb

---
+++ decoder.rb
@@ -0,0 +1,56 @@
+MORSE_MAP = {
+  '.-' => 'A',
+  '-...' => 'B',
+  '-.-.' => 'C',
+  '-..' => 'D',
+  '.' => 'E',
+  '..-.' => 'F',
+  '--.' => 'G',
+  '....' => 'H',
+  '..' => 'I',
+  '.---' => 'J',
+  '-.-' => 'K',
+  '.-..' => 'L',
+  '--' => 'M',
+  '-.' => 'N',
+  '---' => 'O',
+  '.--.' => 'P',
+  '--.-' => 'Q',
+  '.-.' => 'R',
+  '...' => 'S',
+  '-' => 'T',
+  '..-' => 'U',
+  '...-' => 'V',
+  '.--' => 'W',
+  '-..-' => 'X',
+  '-.--' => 'Y',
+  '--..' => 'Z'
+}.freeze
+
+def decode_char(character)
+  MORSE_MAP[character]
+end
+
+puts decode_char(".-")
+
+def decode_word(word)
+  msg_word = ''
+  characters = word.split
+  characters.each do |character|
+    msg_word += decode_char(character)
+  end
+  msg_word
+end
+puts decode_word('-- -.--')
+
+def decode(message)
+  msg = ''
+  words = message.split('   ')
+  words.each do |word|
+    msg += "#{decode_word(word)} "
+  end
+  msg.strip
+end
+puts decode("-- -.--   -. .- -- .")
+
+puts decode('.-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...')
